e6e47f0aa1960e94c5d415aa3f964eeabbe612bb
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-11-10 18:03:50 -0500

    knockback-ish!

--- Character/CharacterWithController.gd
+++ Character/CharacterWithController.gd
@@ -2,27 +2,47 @@ extends CharacterBody3D
 
 
 const SPEED = 40
-const PUSH_FORCE = 50
+const PUSH_FORCE = 5
+var is_being_knockedback := false
+var knockback_dir : Vector3 = Vector3(0, 0 ,0)
 
 @export var goal_marker: Marker3D
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var knockback_timer: Timer
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	var move_vec = (goal_marker.position - position).normalized() * 5 * delta
-	velocity.x = move_vec.x * SPEED
-	velocity.z = move_vec.z * SPEED
+	if is_being_knockedback:
+		knockback_dir.y = 0
+		velocity = move_vec + knockback_dir * PUSH_FORCE
+	else:
+		velocity.x = move_vec.x * SPEED
+		velocity.z = move_vec.z * SPEED
 
 	move_and_slide()
 	apply_collision()
+
+	lerp(knockback_dir, Vector3.ZERO, 0.8)
 
 func apply_collision():
 	# after calling move_and_slide()
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody3D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
+			if is_being_knockedback:
+				return
+			
+			var dir = -c.get_normal()
+			c.get_collider().apply_central_impulse(dir * PUSH_FORCE)
+			knockback_dir = c.get_position().direction_to(global_position)
+			is_being_knockedback = true
+			knockback_timer.start()
+			print("start of knockback")
+			
 	
+
+
+func _on_knockback_timer_timeout():
+	print("end of knockback")
+	is_being_knockedback = false
