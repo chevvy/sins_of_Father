c6a482aeb772067130538a4929f9a06485a95e54
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-12-19 21:10:56 -0500

    full loop babyyy

--- Managers/game_state_manager.gd
+++ Managers/game_state_manager.gd
@@ -3,10 +3,13 @@ extends Node
 signal on_start
 signal on_finish
 
+var player_id_winner = -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	on_finish.connect(on_finish_game)
 
 func on_finish_game(wining_player_id: int):
 	print("player id wins : " + String.num_int64(wining_player_id))
+	player_id_winner = wining_player_id
+	LevelManager.load_win()
 
